3b96af38c925ea0d7ef8464dbb22d12b832cd928
gydud25416 <152379377+gydud25416@users.noreply.github.com>
2024-09-26 14:37:54 +0900

    feat: 카카오 지도 API 호출

--- restapi/src/App.js
+++ restapi/src/App.js
@@ -2,14 +2,24 @@ import { useEffect } from 'react';
 import './App.css';
 import useFetch from './hooks/useFetch';
 
+const { kakao } = window;
 function App() {
   const data = useFetch(`${process.env.REACT_APP_API_URL}?ServiceKey=${process.env.REACT_APP_API_KEY}&searchYearCd=2015&siDo=11&guGun=680&type=json&numOfRows=10&pageNo=1`);
+ 
+
   useEffect(()=>{
-    console.log(data);
-  },[data])
+    const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+    const options = { //지도를 생성할 때 필요한 기본 옵션
+      center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+      level: 3 //지도의 레벨(확대, 축소 정도)
+    };
+    new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+  },[])
+
   return (
     <div className="App">
- 
+      <h1>자전거 사고 다발구역</h1>
+      <div id="map"  ></div>
     </div>
   );
 }
